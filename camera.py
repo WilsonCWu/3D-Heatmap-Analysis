5f7ac6c1fd5b47b79021218e8472870408f5c985
Wilson Wu <wilsoncwu@gmail.com>
2017-01-20 21:01:30 -0500

    2 cameras, adding depth

--- camera.py
+++ camera.py
@@ -1,20 +1,31 @@
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 
-cap = cv2.VideoCapture(1)
+cap1 = cv2.VideoCapture(1)
+cap2 = cv2.VideoCapture(2)
 
 while(True):
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    ret, frame1 = cap1.read()
+    ret, frame2 = cap2.read()
 
     # Our operations on the frame come here
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+
+    stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+    disparity = stereo.compute(gray1,gray2)
 
     # Display the resulting frame
-    cv2.imshow('frame',frame)
+    cv2.imshow('frame1',gray1)
+    cv2.imshow('frame2',gray2)
+    #plt.imshow(disparity, 'gray')
+    #plt.imsave(disparity, 'yolo.png')
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
-cap.release()
+cap1.release()
+cap2.release()
 cv2.destroyAllWindows()
